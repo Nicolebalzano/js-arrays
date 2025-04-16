f15830d3d08debc313e89369fe3768533f132d7e
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-16 16:34:24 +0200

    feat: es 3-4-5-6-7-8 completati

--- script.js
+++ script.js
@@ -27,22 +27,36 @@ teachers.splice(4, 1 , "Patrick");
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers[6];
+console.log(lastTeacher);
+ teachers.splice(6, 1);
+ console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+console.log(firstTeacher);
+ teachers.splice(0, 1);
+ console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.push("Vanessa");
+console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+if (teachers.includes("Fabio")) {
+  const isFabioPresent = "Fabio";
+  console.log(isFabioPresent);
+}
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+let position = teachers.indexOf("Lewis");
+const lewisIndex = position;
+console.log(position);
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
